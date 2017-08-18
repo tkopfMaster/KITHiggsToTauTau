12276eeedd2a70db7f99f6981ea20a2ecedaffc4
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2017-08-18 16:34:57 +0200

    added separate plotlevelfilter configs for MSSM

---
+++ python/data/ArtusConfigs/Run2MSSM/Includes/settingsMinimalPlotlevelFilter_mt.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import re
+import json
+import copy
+import Artus.Utility.jsonTools as jsonTools
+import Kappa.Skimming.datasetsHelperTwopz as datasetsHelperTwopz
+import importlib
+
+def build_config(nickname):
+  config = jsonTools.JsonDict()
+  datasetsHelper = datasetsHelperTwopz.datasetsHelperTwopz("Kappa/Skimming/data/datasets.json")
+  
+  config["PlotlevelFilterExpressionQuantities"] = [
+    "againstElectronVLooseMVA6_2",
+    "extraelec_veto",
+    "againstMuonTight3_2",
+    "extramuon_veto",
+    "byVLooseIsolationMVArun2v1DBoldDMwLT_2",
+    "nDiMuonVetoPairsOS"
+  ]
+  config["PlotlevelFilterExpression"] = "(nDiMuonVetoPairsOS < 0.5)*(extraelec_veto < 0.5)*(extramuon_veto < 0.5)*(againstMuonTight3_2 > 0.5)*(againstElectronVLooseMVA6_2 > 0.5)*(byVLooseIsolationMVArun2v1DBoldDMwLT_2 > 0.5)"
+  
+  return config

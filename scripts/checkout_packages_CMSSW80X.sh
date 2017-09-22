fa3ca4e25c56cca230e95552c056942b824896f6
Rene Caspart <rcaspart@cern.ch>
2017-09-22 14:30:45 +0200

    Fix: adapted Kappa repository in checkoutscript to KIT-CMS

--- scripts/checkout_packages_CMSSW80X.sh
+++ scripts/checkout_packages_CMSSW80X.sh
@@ -21,7 +21,7 @@ if [ "$MODE" = "artus" -o "$MODE" = "both" ]; then
 
     # From Kappa, only the DataFormats are needed
     # Mind that for certain skims, you need exactly the Kappa git tag that has been used for the production
-    git clone https://github.com/KappaAnalysis/Kappa.git
+    git clone https://github.com/KIT-CMS/Kappa.git
     pushd Kappa
     echo docs/ >> .git/info/sparse-checkout
     echo DataFormats/ >> .git/info/sparse-checkout

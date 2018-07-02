abf14696a4e91072b6ae3d74c19a0dd8b8ff6827
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2018-07-02 16:45:00 +0200

    set min plot level filter tau iso to VLoose

--- python/data/ArtusConfigs/Run2Analysis/Includes/settingsMinimalPlotlevelFilter_tt.py
+++ python/data/ArtusConfigs/Run2Analysis/Includes/settingsMinimalPlotlevelFilter_tt.py
@@ -22,9 +22,9 @@ def build_config(nickname):
     "extraelec_veto",
     "againstMuonLoose3_2",
     "extramuon_veto",
-    "byLooseIsolationMVArun2v1DBoldDMwLT_1",
-    "byLooseIsolationMVArun2v1DBoldDMwLT_2"
+    "byVLooseIsolationMVArun2v1DBoldDMwLT_1",
+    "byVLooseIsolationMVArun2v1DBoldDMwLT_2"
   ]
-  config["PlotlevelFilterExpression"] = "(extraelec_veto < 0.5)*(extramuon_veto < 0.5)*(againstMuonLoose3_2 > 0.5)*(againstElectronVLooseMVA6_2 > 0.5)*(byLooseIsolationMVArun2v1DBoldDMwLT_1 > 0.5)*(byLooseIsolationMVArun2v1DBoldDMwLT_2 > 0.5)"
+  config["PlotlevelFilterExpression"] = "(extraelec_veto < 0.5)*(extramuon_veto < 0.5)*(againstMuonLoose3_2 > 0.5)*(againstElectronVLooseMVA6_2 > 0.5)*(byVLooseIsolationMVArun2v1DBoldDMwLT_1 > 0.5)*(byVLooseIsolationMVArun2v1DBoldDMwLT_2 > 0.5)"
   
   return config

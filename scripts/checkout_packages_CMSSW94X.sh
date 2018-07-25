27bde27cf2ac5e4ebd20662eb8a970e6ccb6545c
ArturAkh <artur.akhmetshin@cern.ch>
2018-07-25 20:19:12 +0200

    KITHiggs: update 94X checkout script

--- scripts/checkout_packages_CMSSW94X.sh
+++ scripts/checkout_packages_CMSSW94X.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "$0: Please provide the analysis branch you want to use"
+  exit 1
+fi
+if [ "$2" == "" ]; then
+  echo "$0: Please provide a number of cores for compilation"
+  exit 1
+fi
+
+BRANCH=$1
+CORES=$2
+
 export SCRAM_ARCH=slc6_amd64_gcc630
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
 # set up CMSSW release area
-scramv1 project CMSSW_9_4_6_patch1; pushd CMSSW_9_4_6_patch1/src
+scramv1 project CMSSW_9_4_9; pushd CMSSW_9_4_9/src
 eval `scramv1 runtime -sh`
 
 # JEC
@@ -12,7 +25,7 @@ git cms-addpkg CondFormats/JetMETObjects
 
 # From Kappa, only the DataFormats are needed
 # Mind that for certain skims, you need exactly the Kappa git tag that has been used for the production
-git clone https://github.com/KIT-CMS/Kappa.git
+git clone https://github.com/KIT-CMS/Kappa.git -b $BRANCH
 pushd Kappa
 echo docs/ >> .git/info/sparse-checkout
 echo DataFormats/ >> .git/info/sparse-checkout
@@ -23,19 +36,20 @@ popd
 
 git clone https://github.com/KIT-CMS/KappaTools.git -b master
 
-git clone https://github.com/KIT-CMS/Artus.git
+git clone https://github.com/KIT-CMS/Artus.git -b $BRANCH
 
 # checkout KITHiggsToTauTau CMSSW analysis package
-git clone https://github.com/KIT-CMS/KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau
-#  git clone https://github.com/KIT-CMS/HiggsAnalysis-KITHiggsToTauTau.wiki.git HiggsAnalysis/KITHiggsToTauTau/doc/wiki
+git clone https://github.com/KIT-CMS/KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau -b $BRANCH
 
-# Svfit and HHKinFit
+# quantile mapping package
+git clone https://github.com/KIT-CMS/quantile_mapping
+
+# Svfit
 git clone https://github.com/svfit/ClassicSVfit TauAnalysis/ClassicSVfit
 cd TauAnalysis/ClassicSVfit
 git checkout c78af4dc0f54cdc1c0d4b4fc4879918cfa2527c9
-cd ../..
+cd -
 git clone https://github.com/svfit/SVfitTF TauAnalysis/SVfitTF
-#git clone https://github.com/artus-analysis/HHKinFit2.git -b artus
 
 # Jet2Tau Fakes
 git clone https://github.com/CMS-HTT/Jet2TauFakes.git HTTutilities/Jet2TauFakes
@@ -43,25 +57,17 @@ git clone https://github.com/CMS-HTT/Jet2TauFakes.git HTTutilities/Jet2TauFakes
 # EmuQCD Method
 git clone https://github.com/CMS-HTT/QCDModelingEMu.git HTT-utilities/QCDModelingEMu
 
-# Fit Package for tau polarisation
-#git clone https://github.com/CMSAachen3B/SimpleFits.git -b artus_master
-
 sed '/CombineHarvester/d' ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/BuildFile.xml -i
 
-# needed for error propagation e.g. in the background estimations
-git clone https://github.com/lebigot/uncertainties.git -b 2.4.6.1 HiggsAnalysis/KITHiggsToTauTau/python/uncertainties
-
 # TauTriggerSFs2017 tool
-mkdir TauTriggerSFs2017
-cd TauTriggerSFs2017
-git clone https://github.com/truggles/TauTriggerSFs2017.git -b tauTriggers2017_MCv2_PreReMiniaod
-cd ..
+git clone https://github.com/truggles/TauTriggerSFs2017.git TauTriggerSFs2017/TauTriggerSFs2017 -b tauTriggers2017_MCv2_PreReMiniaod
 
 # Grid-Control
 git clone https://github.com/janekbechtel/grid-control.git
+
 # source ini script, needs to be done in every new shell
 source HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
 
 # compile everything
-scram b -j 4
+scram b -j $CORES
 popd

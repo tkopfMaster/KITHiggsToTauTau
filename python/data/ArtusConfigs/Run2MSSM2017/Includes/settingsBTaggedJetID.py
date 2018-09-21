6427f23de117b04d082ce2b83f697497e15b22ca
Olena Hlushchenko <greyxray@gmail.com>
2018-09-21 15:58:32 +0200

    config:keeping all possible tags in dictionary for an easy changes

--- python/data/ArtusConfigs/Run2MSSM2017/Includes/settingsBTaggedJetID.py
+++ python/data/ArtusConfigs/Run2MSSM2017/Includes/settingsBTaggedJetID.py
@@ -8,6 +8,7 @@ import Artus.Utility.jsonTools as jsonTools
 
 
 def build_config(nickname, **kwargs):
+
     config = jsonTools.JsonDict()
 
     # explicit configuration
@@ -15,28 +16,47 @@ def build_config(nickname, **kwargs):
         "https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsToTauTauWorking2016#b_tagging",
     ]
 
-    # 2017 CSVv2
-    # config["BTagScaleFactorFile"] = "$CMSSW_BASE/src/Artus/KappaAnalysis/data/CSVv2_94XSF_V2_B_F.csv"
-    # config["BTagEfficiencyFile"] = "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-CSVv2_medium.root"
-    # config["BTaggedJetCombinedSecondaryVertexName"] = "pfCombinedInclusiveSecondaryVertexV2BJetTags"
-    # config["BTaggerWorkingPoints"] = [
-    #     "tight:0.9693",
-    #     "medium:0.8838",
-    #     "loose:0.5803",
-    # ]
+    btaggers_collection = {
+        "CSVv2_2017": {
+            "BTagScaleFactorFile": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/CSVv2_94XSF_V2_B_F.csv",
+            "BTagEfficiencyFile": {
+                "tighg": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-CSVv2_tight.root",
+                "medium": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-CSVv2_medium.root",
+                "loose": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-CSVv2_loose.root",
+            },
+            "BTaggerWorkingPoints": [
+                "tight:0.9693",
+                "medium:0.8838",
+                "loose:0.5803",
+            ],
+            "BTaggedJetCombinedSecondaryVertexName": "pfCombinedInclusiveSecondaryVertexV2BJetTags",
+        },
 
-    # TODO: add to the skims: DeepFlavour https://twiki.cern.ch/twiki/bin/view/CMS/DeepJet
-    # Settings for DeepCSV algorithm 94X recommendation (stated to perform better than CSVv2)
-    config["BTagScaleFactorFile"] = "$CMSSW_BASE/src/Artus/KappaAnalysis/data/DeepCSV_94XSF_V3_B_F.csv"
-    config["BTagEfficiencyFile"] = "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-DeepCSV_medium.root"
-    config["BTaggedJetCombinedSecondaryVertexName"] = "pfDeepCSVJetTagsprobbb+pfDeepCSVJetTagsprobb"
-    config["BTaggerWorkingPoints"] = [
-        "tight:0.8001",
-        "medium:0.4941",
-        "loose:0.1522"
-    ]
+        # TODO: add to the skims: DeepFlavour https://twiki.cern.ch/twiki/bin/view/CMS/DeepJet
+        # Settings for DeepCSV algorithm 94X recommendation (stated to perform better than CSVv2)
+        "DeepCSV_2017": {
+            "BTagScaleFactorFile": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/DeepCSV_94XSF_V3_B_F.csv",
+            "BTagEfficiencyFile": {
+                "tighg": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-DeepCSV_tight.root",
+                "medium": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-DeepCSV_medium.root",
+                "loose": "$CMSSW_BASE/src/Artus/KappaAnalysis/data/tagging_efficiencies_march2018_btageff-all_samp-inc-DeepCSV_loose.root",
+            },
+            "BTaggerWorkingPoints": [
+                "tight:0.8001",
+                "medium:0.4941",
+                "loose:0.1522"
+            ],
+            "BTaggedJetCombinedSecondaryVertexName": "pfDeepCSVJetTagsprobbb+pfDeepCSVJetTagsprobb",
+        },
+    }
 
+    btag = btaggers_collection["DeepCSV_2017"]
     config["BTagWPs"] = ["medium"]
+
+    config["BTagScaleFactorFile"] = btag["BTagScaleFactorFile"]
+    config["BTagEfficiencyFile"] = btag["BTagEfficiencyFile"][config["BTagWPs"][0]]
+    config["BTaggedJetCombinedSecondaryVertexName"] = btag["BTaggedJetCombinedSecondaryVertexName"]
+    config["BTaggerWorkingPoints"] = btag["BTaggerWorkingPoints"]
 
     config["BTaggedJetAbsEtaCut"] = 2.5  # 2017 value
     config["ApplyBTagSF"] = True

7bd27d4e6eb5e2af9a6ff4c9c325dd9067adbd0a
Olena Hlushchenko <greyxray@gmail.com>
2018-10-25 15:55:08 +0200

    FES: refer to the nominal pipeline for defining unshifted state,

--- python/data/ArtusConfigs/Run2MSSM2017/et_eleFakeTauES_subanalysis.py
+++ python/data/ArtusConfigs/Run2MSSM2017/et_eleFakeTauES_subanalysis.py
@@ -12,8 +12,25 @@ import os
 import importlib
 
 
+def fshift_dict(shift=None, dm=None):
+    if shift is None or dm is None:
+        print "fshift_dict received wrong parameters"
+        exit(1)
+    config = importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017.nominal").build_config(nickname="pass")["nominal"]
+    config[dm] = 1 + shift / 100.
+    return config
+
+
 def build_config(nickname, **kwargs):
-    etau_fake_es = True if "sub_analysis" in kwargs and kwargs["sub_analysis"] == "etau-fake-es" else False
+    """Produce shifts for e->tau FR ES measurements"""
+    log.debug("Produce shifts for e->tau FR ES measurements")
+    etau_es_shifts = [
+        -4, -3, -2, -1.75, -1.5, -1.25, -1, -0.75, -0.5, -0.25,
+        # 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5,
+        # 2.75, 3, 3.25, 3.5, 3.75, 4, 5, 6, 7, 8,
+        # 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+        # 0
+    ]
 
     config = jsonTools.JsonDict()
     datasetsHelper = datasetsHelperTwopz.datasetsHelperTwopz(os.path.expandvars("$CMSSW_BASE/src/Kappa/Skimming/data/datasets.json"))
@@ -24,67 +41,15 @@ def build_config(nickname, **kwargs):
     isDY = re.search("DY.?JetsToLLM", nickname)
     isEWKZ2Jets = re.search("EWKZ2Jets", nickname)
 
-    # includes
-    includes = []
-    for include_file in includes:
-        analysis_config_module = importlib.import_module(include_file)
-        config += analysis_config_module.build_config(nickname)
+    # Pipelines for producing shapes for calculating the TauElectronFakeEnergyCorrection*
+    if isDY or isEWKZ2Jets or isEmbedded:
 
-    # explicit configuration for pipelins (keys)
-    config["nominal"] = {
-        "JetEnergyCorrectionUncertaintyShift": [0.0],
-    }
+        root_str = lambda x: str(x).replace("-", "neg").replace(".", "p")
 
-    if not isData:
-        config["tauEsOneProngUp"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauEnergyCorrectionOneProng": 1.016,
-        }
-
-        config["tauEsOneProngDown"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauEnergyCorrectionOneProng": 0.999,
-        }
-
-        config["tauEsOneProngOnePiZeroUp"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauEnergyCorrectionOneProngPiZeros": 0.999,
-        }
-
-        config["tauEsOneProngOnePiZeroDown"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauEnergyCorrectionOneProngPiZeros": 0.986,
-        }
-
-        config["tauEsThreeProngUp"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauEnergyCorrectionThreeProng": 0.996,
-        }
-
-        config["tauEsThreeProngDown"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauEnergyCorrectionThreeProng": 0.983,
-        }
-
-    if isDY or isEmbedded:
-        config["eleTauEsOneProngZeroPiZeroUp"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauElectronFakeEnergyCorrectionOneProng": 1.029,
-        }
-
-        config["eleTauEsOneProngZeroPiZeroDown"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauElectronFakeEnergyCorrectionOneProng": 1.019,
-        }
-
-        config["eleTauEsOneProngOnePiZeroUp"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauElectronFakeEnergyCorrectionOneProngPiZeros": 1.086,
-        }
-
-        config["eleTauEsOneProngOnePiZeroDown"] = {
-            "JetEnergyCorrectionUncertaintyShift": [0.0],
-            "TauElectronFakeEnergyCorrectionOneProngPiZeros": 1.066,
-        }
+        for es in etau_es_shifts:
+            config["eleTauEsOneProng_" + root_str(es)] = fshift_dict(es, "TauElectronFakeEnergyCorrectionOneProng")
+            config["eleTauEsOneProngShift_" + root_str(es)] = fshift_dict(es, "TauElectronFakeEnergyCorrectionOneProngShift")
+            config["eleTauEsOneProngPiZerosShift_" + root_str(es)] = fshift_dict(es, "TauElectronFakeEnergyCorrectionOneProngPiZerosShift")
+            config["eleTauEsThreeProngShift_" + root_str(es)] = fshift_dict(es, "TauElectronFakeEnergyCorrectionThreeProngShift")
 
     return config

76a618faf359481bb516b8269496e6b891dbdd7d
Olena Hlushchenko <greyxray@gmail.com>
2018-10-29 22:59:14 +0100

    conf: add missing kwargs

--- python/data/ArtusConfigs/Run2MSSM2017/Includes/settingsJEC.py
+++ python/data/ArtusConfigs/Run2MSSM2017/Includes/settingsJEC.py
@@ -13,16 +13,16 @@ import Artus.Utility.jsonTools as jsonTools
 import importlib
 #import os
 
-def build_config(nickname):
+def build_config(nickname, **kwargs):
   config = jsonTools.JsonDict()
   #datasetsHelper = datasetsHelperTwopz.datasetsHelperTwopz(os.path.expandvars("$CMSSW_BASE/src/Kappa/Skimming/data/datasets.json"))
-  
+
   '''
   JEC documentation:
     https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsToTauTauWorking2017#Jet_MET_uncertainty_treatment
     https://twiki.cern.ch/twiki/bin/view/CMS/JECUncertaintySources#Main_uncertainties_2016_80X
   '''
-  
+
   ## fill config:
   # includes
   includes = [
@@ -30,17 +30,17 @@ def build_config(nickname):
   for include_file in includes:
     analysis_config_module = importlib.import_module(include_file)
     config += analysis_config_module.build_config(nickname)
-  
+
   # explicit configuration
   config["JetEnergyCorrectionParameters"] = []
   if re.search("Run2017|Embedding2017", nickname):
     config["JetEnergyCorrectionUncertaintyParameters"] = "$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/jec/Fall17/Fall17_17Nov2017F_V6_DATA_Uncertainty_AK4PFchs.txt"
   else:
     config["JetEnergyCorrectionUncertaintyParameters"] = "$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/jec/Fall17/Fall17_17Nov2017_V6_MC_Uncertainty_AK4PFchs.txt"
-    
+
   config["JetEnergyCorrectionUncertaintySource"] = ""
   #config["JetEnergyCorrectionUncertaintyShift"] = 0.0
-  
+
   config["JetEnergyCorrectionSplitUncertaintyParameters"] = "$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/jec/Fall17/Fall17_17Nov2017F_V6_DATA_UncertaintySources_AK4PFchs.txt" if re.search("Run2017|Fall17", nickname) else ""
 
 
--- python/data/ArtusConfigs/Run2MSSM2017/Includes/settingsSvfit.py
+++ python/data/ArtusConfigs/Run2MSSM2017/Includes/settingsSvfit.py
@@ -13,11 +13,11 @@ import Artus.Utility.jsonTools as jsonTools
 import importlib
 #import os
 
-def build_config(nickname):
+def build_config(nickname, **kwargs):
   config = jsonTools.JsonDict()
   #datasetsHelper = datasetsHelperTwopz.datasetsHelperTwopz(os.path.expandvars("$CMSSW_BASE/src/Kappa/Skimming/data/datasets.json"))
-  
-  
+
+
   ## fill config:
   # includes
   includes = [
@@ -25,20 +25,20 @@ def build_config(nickname):
   for include_file in includes:
     analysis_config_module = importlib.import_module(include_file)
     config += analysis_config_module.build_config(nickname)
-  
+
   # explicit configuration
-  
+
   # SvfitConsumer config
   config["GenerateSvfitInput"] = False			# actually a flag for the SVFit consumer to stage out to a different file specified via the following file name option; the following 4 options are only relevant if True
   config["SvfitOutFile"] = "SvfitCache.root"		# define different output file name base (which is useful to create inputs for offline SVFit calculations. <channel>_<shift><index> is automatically inserted.
   config["SvfitInputCutOff"] = 2000			# Files can be subdevided in order to avoid too large jobs in the offline SVFit calculation. Specifies max. number of events per file.
   config["UpdateSvfitCache"] = False                    # True: use existing caches and complete them; False: recalculate complete cache
   config["UseFirstInputFileNameForSvfit"] = False	# uses kappa nickname instead of filename specified in "SvfitOutFile"
-  
+
   # SvfitProducer config
   config["SvfitCacheMissBehaviour"] = "recalculate" # Action if SVFit cache is not found. Choose between 'assert': job fails 'undefined': neither runs SVFit nor fails (used when filling SVFit Caches offline) 'recalculate': run SVFit regularly
   config["SvfitIntegrationMethod"] = "MarkovChain"
-  
+
   SvfitCacheFiles = {
   }
   return config

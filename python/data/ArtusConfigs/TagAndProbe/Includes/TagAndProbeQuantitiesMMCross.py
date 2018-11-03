6ef75c1e43c1587315e21e66be710152b3923b5a
Maximilian Burkart <maxiburkart@gmail.com>
2018-11-03 19:13:49 +0100

    Add quantities list for measurement on muon leg of cross trigger.

---
+++ python/data/ArtusConfigs/TagAndProbe/Includes/TagAndProbeQuantitiesMMCross.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import re
+import json
+import Artus.Utility.jsonTools as jsonTools
+import Kappa.Skimming.datasetsHelperTwopz as datasetsHelperTwopz
+
+def build_list():
+  quantities_list = [
+    "run",
+    "lumi",
+    "evt",
+   # "m_vis",
+    "trg_t_IsoMu24",
+    "trg_p_mu20tau27",
+    "trg_p_mu24tau20",
+    "isAntiL1TauMatched_trg_p_mu20tau27",
+    "pt_t", "pt_p",
+    "eta_t", "eta_p",
+    "phi_t", "phi_p",
+    "iso_t", "iso_p",
+    "id_t",
+    "id_p",
+    "m_ll"
+    ]
+
+  return quantities_list

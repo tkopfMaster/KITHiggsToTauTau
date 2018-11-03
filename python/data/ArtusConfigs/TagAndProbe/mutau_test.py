ef21db06b0e9c710172e5c70e36e88ed114c436c
Maximilian Burkart <maxiburkart@gmail.com>
2018-11-03 19:09:41 +0100

    Update config for mu tau selection.
    Remove config keys not needed any more. Add config keys for pT dependent
    trigger matching and L1Tau matching and producer for L1Tau matching.
    Additionally add filter to reduce number of pairs quantities are written
    from to the most isolated per event.

--- python/data/ArtusConfigs/TagAndProbe/mutau_test.py
+++ python/data/ArtusConfigs/TagAndProbe/mutau_test.py
@@ -48,16 +48,6 @@ def build_config(nickname, **kwargs):
   config["MinNTaus"] = 1
   config["MaxNLooseElectrons"] = 0
   config["NMuons"] = 1
-  # HltPaths_comment: The first path must be the single lepton trigger. A corresponding Pt cut is implemented in the Run2DecayChannelProducer..
-  if re.search("(Run201|Embedding201|Summer1|Fall1)", nickname): config["HltPaths"] = [
-          "HLT_IsoMu24",
-          "HLT_IsoMu27",
-          "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",
-          "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1",
-          "HLT_IsoMu24_eta2p1_MediumChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg_CrossL1",
-          "HLT_IsoMu24_eta2p1_TightChargedIsoPFTau35_Trk1_eta2p1_Reg_CrossL1",
-          "HLT_IsoMu24_eta2p1_TightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg_CrossL1",
-  ]
 
   #config["TauID"] = "TauIDRecommendation13TeV"
   config["TauID"] = "none"
@@ -70,49 +60,28 @@ def build_config(nickname, **kwargs):
   config["DeltaRTriggerMatchingTaus"] = 0.5
   config["DeltaRTriggerMatchingMuons"] = 0.5
   config["DiTauPairIsTauIsoMVA"] = True
-  config["DiTauPairLepton1LowerPtCuts"] = [
-          "HLT_IsoMu24_v:25.0",
-          "HLT_IsoMu27_v:28.0",
-          "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1_v:21.0",
-  ]
-  config["DiTauPairLepton2LowerPtCuts"] = [
-          "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1_v:32.0",
-  ]
-  config["DiTauPairLepton2UpperEtaCuts"] = [
-          "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1_v:2.1",
-          "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v:2.1",
-  ]
+
   config["CheckTagTriggerMatch"] = [
       "trg_singlemuon_27",
   ]
   config["CheckProbeTriggerMatch"] = [
       "trg_singletau_trailing",
-
+      "trg_singletau_leading",
       "trg_crossmuon_mu20tau27",
       "trg_monitor_mu24tau35_medium_tightID",
       "trg_monitor_mu24tau35_tight",
       "trg_monitor_mu24tau35_tight_tightID",
+      "trg_monitor_mu20tau27",
   ]
   config["HLTBranchNames"] = [
-      "trg_singlemuon_24:HLT_IsoMu24_v",
       "trg_singlemuon_27:HLT_IsoMu27_v",
       "trg_crossmuon_mu20tau27:HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1_v",
-      "trg_singleelectron_27:HLT_Ele27_WPTight_Gsf_v",
-      "trg_singleelectron_32:HLT_Ele32_WPTight_Gsf_v",
-      "trg_singleelectron_32_fallback:HLT_Ele32_WPTight_Gsf_DoubleL1EG_v",
-      "trg_singleelectron_35:HLT_Ele35_WPTight_Gsf_v",
-      "trg_crossele_ele24tau30:HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1_v",
-      "trg_doubletau_35_tightiso_tightid:HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg_v",
-      "trg_doubletau_40_mediso_tightid:HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg_v",
-      "trg_doubletau_40_tightiso:HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg_v",
       "trg_singletau_leading:HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v",
       "trg_singletau_trailing:HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v",
-      "trg_muonelectron_mu12ele23:HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
-      "trg_muonelectron_mu23ele12:HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
-      "trg_muonelectron_mu8ele23:HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v",
       "trg_monitor_mu24tau35_medium_tightID:HLT_IsoMu24_eta2p1_MediumChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg_CrossL1_v",
       "trg_monitor_mu24tau35_tight:HLT_IsoMu24_eta2p1_TightChargedIsoPFTau35_Trk1_eta2p1_Reg_CrossL1_v",
       "trg_monitor_mu24tau35_tight_tightID:HLT_IsoMu24_eta2p1_TightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg_CrossL1_v",
+      "trg_monitor_mu20tau27:HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1_v",
   ]
   config["TauTrigger2017InputOld"] = "$CMSSW_BASE/src/TauTriggerSFs2017/TauTriggerSFs2017/data/tauTriggerEfficiencies2017.root"
   config["TauTrigger2017Input"] = "$CMSSW_BASE/src/TauTriggerSFs2017/TauTriggerSFs2017/data/tauTriggerEfficiencies2017_New.root"
@@ -131,7 +100,6 @@ def build_config(nickname, **kwargs):
 
   config["EventWeight"] = "eventWeight"
   config["MuonTriggerFilterNames"] = [
-          "HLT_IsoMu24_v:hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07",
           "HLT_IsoMu27_v:hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07",
           "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1_v:hltL3crIsoL1sMu18erTau24erIorMu20erTau24erL1f0L2f10QL3f20QL3trkIsoFiltered0p07",
           "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1_v:hltOverlapFilterIsoMu20LooseChargedIsoPFTau27L1Seeded",
@@ -155,6 +123,17 @@ def build_config(nickname, **kwargs):
           "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v:hltPFTau180TrackPt50LooseAbsOrRelMediumHighPtRelaxedIsoIso",
           "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v:hltSelectedPFTau180MediumChargedIsolationL1HLTMatched"
     ]
+  config["CheckTriggerLowerPtCutsByHltNick"] = [
+          "trg_monitor_mu20tau27:30.0",
+          "trg_monitor_mu24tau35_medium_tightID:40.0",
+          "trg_monitor_mu24tau35_tight:40.0",
+    ]
+  config["TauTriggerCheckAdditionalL1TauMatchLowerPtCut"] = [
+          "trg_monitor_mu24tau35_medium_tightID:32.0",
+          "trg_monitor_mu24tau35_tight:32.0",
+          "trg_monitor_mu24tau35_tight_tightID:32.0",
+          "trg_monitor_mu20tau27:26.0",
+    ]
 
   #TriggerMatchingProducers
   config["InvalidateNonMatchingElectrons"] = False
@@ -165,36 +144,26 @@ def build_config(nickname, **kwargs):
   config["DirectIso"] = True
 
   config["Quantities"] = importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.TagAndProbe.Includes.TagAndProbeQuantitiesMT").build_list()
-  #config["Quantities"] = importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.NewTagAndProbe.Includes.trigQuantities").build_list()
-  #config["Quantities"].extend(importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Includes.weightQuantities").build_list())
-  #config["Quantities"].extend(importlib.import_module("HiggsAnalysis.KITHiggsToTauTau.data.ArtusConfigs.Run2MSSM2017.Includes.lheWeights").build_list())
 
-  config["Processors"] =   []#                                  ["producer:MuonCorrectionsProducer"] if isEmbedded else []
-#  if not (isEmbedded):           config["Processors"].append( "producer:ElectronCorrectionsProducer")
+  config["Processors"] =   []
 
   config["Processors"].extend((                               "producer:HttValidLooseElectronsProducer",   # Electrons for electron veto
                                                               "filter:MaxLooseElectronsCountFilter",
                                                               "producer:HttValidLooseMuonsProducer",
                                                               "filter:MaxLooseMuonsCountFilter",
-                                                              #"producer:HltProducer",
                                                               "producer:MetSelector",
                                                               "producer:ValidMuonsProducer",
                                                               "filter:ValidMuonsFilter",
                                                               "producer:MuonTriggerMatchingProducer",
                                                               "filter:MuonsCountFilter",
-                                                              #"producer:HttValidVetoMuonsProducer",
                                                               "producer:ValidTausProducer",
                                                               "filter:ValidTausFilter",
                                                               "producer:TauTriggerMatchingProducer",
+                                                              "producer:TauL1TauTriggerMatchingProducer",
                                                               "filter:MinTausCountFilter",
                                                               "producer:ValidBTaggedJetsProducer",
-                                                              #"producer:ValidMTPairCandidatesProducer",
                                                               "producer:NewMTTagAndProbePairCandidatesProducer",
-                                                              #"filter:ValidDiTauPairCandidatesFilter",
-                                                              #"producer:Run2DecayChannelProducer",
-                                                              #"producer:DiVetoMuonVetoProducer",
-                                                              #"producer:TaggedJetCorrectionsProducer",
-                                                              #"producer:DiLeptonQuantitiesProducer",
+                                                              "filter:ValidDiTauPairCandidatesFilter",
                                                               ))
 
   config["Processors"].append(                                "producer:EventWeightProducer")
